6bf9d7179f5ea64a6103c967cf5fa240321141dd
James Polley <jp@jamezpolley.com>
2019-06-17 14:23:19 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,4 @@ Bundler.require
 
 url = "https://jamezpolley.github.io/atdis_utcdatetime_test/atdis/1.0"
 
-ATDISPlanningAlertsFeed.save(url, {lodgement_date_start: "2018-01-01", lodgement_end_date: "2018-03-01"})
+ATDISPlanningAlertsFeed.save(url, "Sydney", {lodgement_date_start: "2018-01-01", lodgement_end_date: "2018-03-01"})
